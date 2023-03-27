4a8189899140f31575f99dcdd5bcc04775690008
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-03-28 02:47:28 +0530

    todoList Final Commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 // import UseState from "./components/hooks/UseState";
 import UseEffect from "./components/hooks/UseEffect";
 import UseReducer from "./components/hooks/UseReducer";
+import Todo from "./components/todoReact/todo";
+import './components/todoReact/style.css';
 
 const App = () => {
   return (
     <>
-      <UseReducer></UseReducer>
+      <Todo></Todo>
     </>
   );
 };
